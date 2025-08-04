2f8046651e42a1fb707d0273950360c4db905a55
Nandhini172004 <nandinigowda174@gmail.com>
2025-08-04 12:36:44 +0530

    Add files via upload

---
+++ numpy1.py
@@ -0,0 +1,16 @@
+import numpy as np
+a=np.array([1,2,3])
+b=np.array([4,5,6])
+sum_result=a+b
+product_result=a*b
+mean_value=np.mean(a)
+max_value=np.max(b)
+print("array a:",a)
+print("array b:",b)
+print("sum:",sum_result)
+print("product:",product_result)
+print("mean of a:",mean_value)
+print ("max of b:",max_value)
+
+
+
